0f6721895570f61f7c40caed15a3417559b598fd
Mikko Salkolahti <salkolahti@users.noreply.github.com>
2014-07-25 11:11:53 +0300

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,8 +12,8 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
+	var defaultKey		= 'S93Nh73GPnnvtYrUYR86L3szc6thN46y22gQRMjnt0CPsila', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1411868071], // Comma separated array of Xively Feed ID numbers
 		applicationName	= '', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
